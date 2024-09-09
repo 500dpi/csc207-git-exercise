712bdcd26744f4671f4064072c1719d56ae06901
500dpi <jaljaasa@grinnell.edu>
2024-09-09 18:30:38 -0500

    Exercise 12

--- csc207-git-exercise/HelloWorld.java
+++ csc207-git-exercise/HelloWorld.java
@@ -3,7 +3,8 @@ import java.io.PrintWriter;
 public class HelloWorld {
   public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    pen.println ("Hello, GitHub (edited from GitHUb)!");
+    pen.println ("Hello, GitHub (edited from GitHub)!");
+    pen.println("Conflicting Changes");
     pen.flush();
     pen.close();
   } // main(String[])
